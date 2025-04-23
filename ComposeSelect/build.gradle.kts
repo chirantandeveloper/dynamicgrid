9a7642337713511bb6140ae3f764e87d8907a9f7
unknown <mygamingac91@gmail.com>
2025-04-24 00:44:01 +0530

    jitpack is added

--- ComposeSelect/build.gradle.kts
+++ ComposeSelect/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -53,4 +54,18 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.github.chirantandeveloper"
+                artifactId = "ComposeSelect"
+                version = "1.0"
+            }
+        }
+    }
 }
