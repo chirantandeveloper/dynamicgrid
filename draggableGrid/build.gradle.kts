956b231a78ffa8040053dced5799ba025a5485ad
unknown <mygamingac91@gmail.com>
2025-08-20 00:00:00 +0530

    gradle upgrade

--- draggableGrid/build.gradle.kts
+++ draggableGrid/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+   // id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -24,11 +26,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
     buildFeatures {
@@ -50,4 +52,18 @@ dependencies {
     implementation ("androidx.compose.runtime:runtime-livedata:1.7.3")
     implementation ("androidx.compose.ui:ui:1.7.3")
     implementation ("androidx.compose.material:material:1.7.3")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.github.chirantandeveloper"
+                artifactId = "dynamicgrid"
+                version = "1.0.0"
+            }
+        }
+    }
 }
